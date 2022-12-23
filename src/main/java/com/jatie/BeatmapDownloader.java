233b27881bc195860ef016bf9cfaa2730d2c6b37
Jatie <thomasmanthos@hotmail.com>
2022-12-23 15:36:18 +1100

    remove dupe code

--- src/main/java/com/jatie/BeatmapDownloader.java
+++ src/main/java/com/jatie/BeatmapDownloader.java
@@ -287,23 +287,22 @@ public class BeatmapDownloader {
             System.out.println("Enter either a single year or a year range you want to begin fetching beatmaps from (between 2007 to " + currentYear + " in the format 'YYYY' for single year and 'YYYY-YYYY' for a range)");
             System.out.print("EG: '2019' will fetch all beatmaps from 2019 and '2013-2020' will fetch all beatmaps between 2013 and 2020: ");
             String yearRange = SCANNER.nextLine();
+            System.out.print("\n");
             if (yearRange.matches("\\d{4}-\\d{4}")) { // Range
                 String[] yearRangeSplit = yearRange.split("-");
                 int yearStart = Integer.parseInt(yearRangeSplit[0]);
                 int yearEnd = Integer.parseInt(yearRangeSplit[1]);
                 if (yearStart >= 2007 && yearEnd <= currentYear && yearEnd > yearStart) {
-                    System.out.print("\n");
                     return new String[]{yearRangeSplit[0] + "-01-01", Integer.toString(yearEnd + 1)};
                 }
             }
             else if (yearRange.matches("\\d{4}")) { // Single year
                 int year = Integer.parseInt(yearRange);
                 if (year >= 2007 && year <= currentYear) {
-                    System.out.print("\n");
                     return new String[]{yearRange + "-01-01", Integer.toString(year + 1)};
                 }
             }
-            System.out.println("\nYear input is invalid!");
+            System.out.println("Year input is invalid!");
         }
     }
 
